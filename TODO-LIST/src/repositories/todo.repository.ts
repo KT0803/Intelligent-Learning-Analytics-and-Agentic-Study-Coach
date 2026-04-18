53943689cdf74e01bb2acde419419ff9c0c2ba7a
Jatin Bisen <jatinnbisen@gmail.com>
2026-04-18 17:19:11 +0000

    changes had to be made for GEMINI API and payload issues

--- TODO-LIST/src/repositories/todo.repository.ts
+++
@@ -1,8 +0,0 @@
-import { BaseRepository } from "./base.repository";
-import { ITodo, TodoModel } from "../models/todo.model";
-
-export class TodoRepository extends BaseRepository<ITodo> {
-    constructor() {
-        super(TodoModel);
-    }
-}
